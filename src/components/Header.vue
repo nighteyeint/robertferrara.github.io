528c5f90b3cb865330fd328dc1359053ef3996fa
Rob <robert@lifeinmobile.com>
2024-05-14 13:12:44 -0400

    Test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,8 @@
     <nav>
       <ul>
         <li><a href="/">HOME</a></li>
-        <li><a href="/about">ABOUT</a></li>
-        <li><a href="/contact">CONTACT</a></li>
+        <li><a href="about">ABOUT</a></li>
+        <li><a href="contact">CONTACT</a></li>
       </ul>
     </nav>
   </header>
